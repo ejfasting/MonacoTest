8170ea9d0b6c49a70bc728c1fd7ed730e0888dab
Eivind Fasting <eivind.fasting@amesto.no>
2024-01-24 10:19:57 +0100

    Updated with SimpleSample

---
+++ src/client.ts
@@ -0,0 +1,50 @@
+
+import { MonacoLanguageClient, initServices, useOpenEditorStub } from 'monaco-languageclient';
+import { BrowserMessageReader, BrowserMessageWriter } from 'vscode-languageserver-protocol/browser.js';
+import { CloseAction, ErrorAction, MessageTransports } from 'vscode-languageclient';
+
+const languageId = 'statemachine';
+
+export const setupClient = async () => {
+    console.log('Setting up Statemachine client configuration ...');
+
+    function createLanguageClient(transports: MessageTransports): MonacoLanguageClient {
+      return new MonacoLanguageClient({
+          name: 'Statemachine Client',
+          clientOptions: {
+              // use a language id as a document selector
+              documentSelector: [{ language: languageId }],
+              // disable the default error handler
+              errorHandler: {
+                  error: () => ({ action: ErrorAction.Continue }),
+                  closed: () => ({ action: CloseAction.DoNotRestart })
+              }
+          },
+          // create a language client connection to the server running in the web worker
+          connectionProvider: {
+              get: () => {
+                  return Promise.resolve(transports);
+              }
+          }
+      });
+    }
+    const langiumWorkerUrl = new URL('./server.js', '127.0.0.1:5500/clients/monaco/').href;
+      const worker = new Worker(langiumWorkerUrl, {
+          type: 'module',
+          name: 'Custom LS'
+      });
+      const reader = new BrowserMessageReader(worker);
+      const writer = new BrowserMessageWriter(worker);
+      const languageClient = createLanguageClient({ reader, writer });
+      languageClient.start();
+      reader.onClose(() => languageClient.stop());
+
+};
+
+export const startClient = async () => {
+  try {
+      await setupClient();
+  } catch (e) {
+      console.error(e);
+  }
+};
