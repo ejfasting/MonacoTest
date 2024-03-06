64b349ce78014ffece8e7c3cdc851d263257285e
ejfasting <ejfasting@gmail.com>
2024-03-06 11:20:34 +0100

    updated with vite

---
+++ vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite';
+// import vsixPlugin from '@codingame/monaco-vscode-rollup-vsix-plugin'
+
+// // https://vitejs.dev/config/
+export default defineConfig({
+    plugins: []
+});
