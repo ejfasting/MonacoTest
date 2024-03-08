5f8cc56e4faab41b8f8fc9e8e316e51a49252895
ejfasting <ejfasting@gmail.com>
2024-03-08 20:52:30 +0100

    attempt to configure the workerConfig differently

--- src/main.ts
+++ src/main.ts
@@ -32,10 +32,10 @@ const languageId = 'jsfso';
 export type WorkerLoader = () => Worker
 const workerLoaders: Partial<Record<string, WorkerLoader>> = {
   editorWorkerService: () => new Worker(new URL('monaco-editor/esm/vs/editor/editor.worker.js', import.meta.url), { type: 'module' }),
-  textMateWorker: () => new Worker(new URL('@codingame/monaco-vscode-textmate-service-override/worker', import.meta.url), { type: 'module' }),
-  outputLinkComputer: () => new Worker(new URL('@codingame/monaco-vscode-output-service-override/worker', import.meta.url), { type: 'module' }),
-  languageDetectionWorkerService: () => new Worker(new URL('@codingame/monaco-vscode-language-detection-worker-service-override/worker', import.meta.url), { type: 'module' }),
-  notebookEditorWorkerService: () => new Worker(new URL('@codingame/monaco-vscode-notebook-service-override/worker', import.meta.url), { type: 'module' })
+  // textMateWorker: () => new Worker(new URL('@codingame/monaco-vscode-textmate-service-override/worker', import.meta.url), { type: 'module' }),
+  // outputLinkComputer: () => new Worker(new URL('@codingame/monaco-vscode-output-service-override/worker', import.meta.url), { type: 'module' }),
+  // languageDetectionWorkerService: () => new Worker(new URL('@codingame/monaco-vscode-language-detection-worker-service-override/worker', import.meta.url), { type: 'module' }),
+  // notebookEditorWorkerService: () => new Worker(new URL('@codingame/monaco-vscode-notebook-service-override/worker', import.meta.url), { type: 'module' })
 };
 
 window.MonacoEnvironment = {
--- src/tools/extHostWorker.ts
+++ src/tools/extHostWorker.ts
@@ -1,7 +1,13 @@
 import type { WorkerConfig } from '@codingame/monaco-vscode-extensions-service-override';
 import { Worker } from './fakeWorker';
 
-const fakeWorker = new Worker(new URL('vscode/workers/extensionHost.worker', import.meta.url), { type: 'module' });
+const jsfsoWorkerUrl = new URL('vscode/workers/extensionHost.worker', import.meta.url);
+const fakeWorker = new Worker(jsfsoWorkerUrl, {
+    type: 'module',
+    name: 'jsfso LS'
+});
+
+//const fakeWorker = new Worker(new URL('vscode/workers/extensionHost.worker', import.meta.url), { type: 'module' });
 
 export const workerConfig: WorkerConfig = {
     url: fakeWorker.url.toString(),
